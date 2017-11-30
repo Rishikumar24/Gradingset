9cbf808c833168a432038bcee0daf541fc4a163e
Rishikumar24 <33508113+Rishikumar24@users.noreply.github.com>
2017-11-30 15:22:09 +0530

    Add files via upload

---
+++ Grading set/top_media_type.sql
@@ -0,0 +1,7 @@
+/*27.top_media_type.sql: Provide a query that shows the most purchased Media Type.*/
+select  m.name,count(i.invoicelineid) as count
+from  invoiceline i,track t,mediatype m
+where i.trackid=t.trackid and t.mediatypeid=m.mediatypeid
+group by m.mediatypeid
+order by count desc
+limit 0,1;
---
+++ Grading set/tracks_no_id.sql
@@ -0,0 +1,3 @@
+/*16.tracks_no_id.sql: Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre.*/
+select t.name,a.title,m.name,g.name from track t,album a,mediatype m,genre g where 
+t.albumid=a.albumid and t.mediatypeid=m.mediatypeid and t.genreid=g.genreid;
